ca90b8dc199dacd2f589109a592791080ad0d21b
AAndreazza <alanaandreazza26@hotmail.com>
2024-02-01 16:10:19 -0300

    pagina principal ok

--- CadastroItens/CadastroItens.js
+++ CadastroItens/CadastroItens.js
@@ -6,32 +6,6 @@ function Cadastrar() {
   window.location = "../CadastroItens/CadastroItens.htm";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    var menuToggle = document.querySelector(".menu-toggle");
-    var menuLateral = document.getElementById("menuLateral");
-  
-    menuToggle.addEventListener("click", function () {
-      menuLateral.classList.toggle("active");
-    });
-  
-    // Verifica o tamanho da tela ao carregar a página e define a visibilidade inicial
-    checkWindowSize();
-    
-    // Atualiza a visibilidade do menu lateral quando a tela é redimensionada
-    window.addEventListener("resize", function () {
-      checkWindowSize();
-    });
-  
-    function checkWindowSize() {
-      if (window.innerWidth <= 600) {
-        menuToggle.style.display = "block";
-        menuLateral.style.display = menuLateral.classList.contains("active") ? "block" : "none";
-      } else {
-        menuToggle.style.display = "none";
-        menuLateral.style.display = "flex";
-      }
-    }
-  });
   
   
 
