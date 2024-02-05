366cbbf3f41437340220ccda62e6b3611b9cdd14
Felipe Cizanosky <111258259+Cizanosky@users.noreply.github.com>
2024-02-05 16:12:56 -0300

    Update BancoDeDados.sql

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -1,16 +1,16 @@
 -- Table: TelaCadastro_usuario
-CREATE TABLE TelaCadastro_usuario (
-    cpf char(11)  NOT NULL,
+CREATE TABLE Usuario (
+    cpf char(11)  NOT NULL UNIQUE,
     nome varchar(100)  NOT NULL,
-    email varchar(50)  NOT NULL,
-    telefone char(15)  NOT NULL,
+    email varchar(50)  NOT NULL UNIQUE,
+    telefone char(15)  NOT NULL UNIQUE,
     dt_nascimento date  NOT NULL,
     senha varchar(15)  NOT NULL,
     CONSTRAINT TelaCadastro_usuario_pk PRIMARY KEY (cpf)
 );
 
 -- Table: cadastroItem
-CREATE TABLE cadastroItem (
+CREATE TABLE Item (
     cd_item int AUTO_INCREMENT NOT NULL,
     produto varchar(50)  NOT NULL,
     data_compra date  NOT NULL,
@@ -18,12 +18,14 @@ CREATE TABLE cadastroItem (
     quantidade int,
     descricao varchar(300),
     sg_setor char(2)  NOT NULL,
-    TelaCadastro_usuario_cpf char(11)  NOT NULL,
-    CONSTRAINT cadastroItem_pk PRIMARY KEY (cd_item)
+    usuario_cpf char(11)  NOT NULL,
+    CONSTRAINT cadastroItem_pk PRIMARY KEY (cd_item),
+    FOREIGN KEY item(usuario_cpf) REFERENCES usuario(cpf)
 );
 
 -- foreign keys
 -- Reference: cadastroItem_TelaCadastro_usuario (table: cadastroItem)
-ALTER TABLE cadastroItem ADD CONSTRAINT cadastroItem_TelaCadastro_usuario FOREIGN KEY cadastroItem_TelaCadastro_usuario (TelaCadastro_usuario_cpf)
-    REFERENCES TelaCadastro_usuario (cpf);
-ALTER TABLE cadastroItem MODIFY COLUMN sg_setor INT COMMENT '01 - Moradia, 02 - Lazer, 03 - Impostos, 04 - Financiamento, 05 - Educacao, 06 - Cuidados Pessoais, 07 - Mercado, 08 - Poupanca ou Investimentos, 09 - Saude, 10 - Transporte, 11 - Vestuario, 12 - Outros';
+ALTER TABLE item ADD CONSTRAINT cadastroItem_TelaCadastro_usuario FOREIGN KEY item (TelaCadastro_usuario_cpf)
+    REFERENCES Usuario (cpf);
+ALTER TABLE item
+	CHANGE COLUMN sg_setor CHAR(2) NOT NULL COMMENT '01 - Moradia, 02 - Lazer, 03 - Impostos, 04 - Financiamento, 05 - Educacao, 06 - Cuidados Pessoais, 07 - Mercado, 08 - Poupanca ou Investimentos, 09 - Saude, 10 - Transporte, 11 - Vestuario, 12 - Outros' COLLATE 'latin1_swedish_ci' AFTER descricao;
