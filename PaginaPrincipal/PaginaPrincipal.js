19318efb0938b188faf80fec732df528e572914b
LetFruet <letfruet@furb.br>
2024-02-14 14:32:30 -0300

    .

--- PaginaPrincipal/PaginaPrincipal.js
+++ PaginaPrincipal/PaginaPrincipal.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Este bloco de código será executado após o carregamento do DOM
-    const nomeUsuario = sessionStorage.getItem('nomeUsuario');
-    const elementoNomeUsuario = document.getElementById('link-usuario');
-  
-    if (elementoNomeUsuario) {
-      elementoNomeUsuario.innerText = nomeUsuario;
-    }
-  });
+  // Este bloco de código será executado após o carregamento do DOM
+  const nomeUsuario = sessionStorage.getItem('nomeUsuario');
+  const elementoNomeUsuario = document.getElementById('link-usuario');
 
-  function hideMenuOnSmallScreens() {
-    var screenWidth = window.innerWidth;
-    var menuLateral = document.getElementById('menuLateral');
-    var menuSUPERIOR = document.getElementById('menuSUPERIOR');
-   
+  if (elementoNomeUsuario) {
+    elementoNomeUsuario.innerText = nomeUsuario;
+  }
+});
 
-    if (screenWidth <= 460) { // Se a largura da tela for 460 pixels ou menos
-        menuLateral.style.display = 'none'; // Oculta o menu lateral
-        menuSUPERIOR.style.display = 'block'; 
-        
-    } else {
-        menuLateral.style.display = 'block'; // Exibe o menu lateral
-        menuSUPERIOR.style.display = 'none'; 
-    }
+function hideMenuOnSmallScreens() {
+  var screenWidth = window.innerWidth;
+  var menuLateral = document.getElementById('menuLateral');
+  var menuSUPERIOR = document.getElementById('menuSUPERIOR');
+
+  if (screenWidth <= 460) { // Se a largura da tela for 460 pixels ou menos
+    menuLateral.style.display = 'none'; // Oculta o menu lateral
+    menuSUPERIOR.style.display = 'block';
+
+  } else {
+    menuLateral.style.display = 'block'; // Exibe o menu lateral
+    menuSUPERIOR.style.display = 'none';
+  }
 }
 
 window.onload = hideMenuOnSmallScreens;
