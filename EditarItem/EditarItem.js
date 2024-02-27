2e4519c7397ee29bf96ec1a05fb82de2d762ac4b
AAndreazza <alanaandreazza26@hotmail.com>
2024-02-27 14:28:00 -0300

    ALTERACOES FINAIS

--- EditarItem/EditarItem.js
+++ EditarItem/EditarItem.js
@@ -1,48 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const nomeUsuario = sessionStorage.getItem('nomeUsuario');
     const elementoNomeUsuario = document.getElementById('link-usuario');
     const elementoNomeUsuario2 = document.getElementById('link-usuario2');
-
+  
     elementoNomeUsuario.innerText = nomeUsuario;
     elementoNomeUsuario2.innerText = nomeUsuario;
-
+  
     const usuarioId = sessionStorage.getItem('usuarioId');
     carregarItens(usuarioId, 1, 0, 0); // Adicione os parâmetros para mês e setor
     mostrarSaldoTotal(usuarioId, 0, 0);
-
+  
     // Adicione o código para lidar com os filtros
     const filtroMes = document.getElementById('filtro-mes');
     const filtroSetor = document.getElementById('filtro-categoria');
-
+  
     filtroMes.addEventListener('change', function () {
         filtroMesSelecionado = parseInt(filtroMes.value);
         carregarItens(sessionStorage.getItem('usuarioId'), 1, filtroMesSelecionado, parseInt(filtroSetor.value));
         mostrarSaldoTotal(usuarioId, filtroMesSelecionado, parseInt(filtroSetor.value));
     });
-
+  
     filtroSetor.addEventListener('change', function () {
         filtroSetorSelecionado = parseInt(filtroSetor.value);
         carregarItens(sessionStorage.getItem('usuarioId'), 1, parseInt(filtroMes.value), filtroSetorSelecionado);
         mostrarSaldoTotal(usuarioId, parseInt(filtroMes.value), filtroSetorSelecionado);
     });
-
-
-});
-
-
-
-let filtroMesSelecionado;
-let filtroSetorSelecionado;
-let totalPages = 0;
-let currentPage = 1;
-const itemsPerPage = 10; // Número de itens por página
-
-
-function mostrarSaldoTotal(usuarioId, mes, setor) {
+  
+  
+  });
+  
+  
+  
+  let filtroMesSelecionado;
+  let filtroSetorSelecionado;
+  let totalPages = 0;
+  let currentPage = 1;
+  const itemsPerPage = 10; // Número de itens por página
+  
+  
+  function mostrarSaldoTotal(usuarioId, mes, setor) {
     const endpoint = `http://localhost:8080/item/saldo/${usuarioId}`;
     let queryParams = '';
-
+  
     if (mes !== undefined && mes !== 0) {
         filtroMesSelecionado = mes;
         queryParams += `?mes=${mes}`;
@@ -51,9 +50,9 @@ function mostrarSaldoTotal(usuarioId, mes, setor) {
         filtroSetorSelecionado = setor;
         queryParams += `${queryParams ? '&' : '?'}setor=${setor}`;
     }
-
+  
     const urlCompleta = `${endpoint}${queryParams}`;
-
+  
     fetch(urlCompleta)
         .then(response => {
             if (!response.ok) {
@@ -72,15 +71,15 @@ function mostrarSaldoTotal(usuarioId, mes, setor) {
         .catch(error => {
             console.error('Erro:', error.message);
         });
-}
-
-
-// Função para carregar os itens na tabela
-function carregarItens(usuarioId, page, mes, setor) {
+  }
+  
+  
+  // Função para carregar os itens na tabela
+  function carregarItens(usuarioId, page, mes, setor) {
     const tabelaCorpo = document.getElementById('tabelaCorpo');
     tabelaCorpo.innerHTML = '';
     let endpoint = `http://localhost:8080/item/usuario/${usuarioId}?page=${page - 1}&size=${itemsPerPage}`;
-
+  
     if (mes !== undefined && mes !== 0) {
         endpoint += `&mes=${mes}`;
     }
@@ -94,21 +93,21 @@ function carregarItens(usuarioId, page, mes, setor) {
             }
             return response.json();
         })
-
+  
         .then(data => {
             // Verifique se o objeto data possui uma propriedade chamada 'content'
             if (data.hasOwnProperty('content')) {
                 const itens = data.content;
-
+  
                 // Agora você pode iterar sobre os itens
                 itens.forEach(item => {
                     tabelaCorpo.appendChild(adicionarLinha(item.produto, item.dataCompra, item.valor, item.descricao, item.cdItem));
                 });
-
+  
                 totalPages = data.totalPages;
                 currentPage = page;
                 document.getElementById('page-number').innerText = currentPage;
-
+  
             } else {
                 console.error("Resposta da API não possui a propriedade 'content'.");
             }
@@ -116,9 +115,9 @@ function carregarItens(usuarioId, page, mes, setor) {
         .catch(error => {
             console.error('Ocorreu um erro ao tentar obter os itens:', error.message);
         });
-}
-
-function adicionarLinha(produto, data, valor, descricao, id) {
+  }
+  
+  function adicionarLinha(produto, data, valor, descricao, id) {
     const novaLinha = document.createElement('tr');
     const valorFormatted = parseFloat(valor).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
     novaLinha.innerHTML = `
@@ -141,45 +140,45 @@ function adicionarLinha(produto, data, valor, descricao, id) {
                         </td> 
                         <td>
                               <div class="d-flex justify-content-center align-items-center">
-                              <a href="../EditarItem/EditarItem.html?id=${id}"><button class="botao-alterar-item" data-item-id="${id}"><i id="icon-mais-descricao"
-                              class="bi bi-pencil"></i>
-                                 </button></a>
+                                 <button class="botao-alterar-item" data-item-id="${id}"><i id="icon-mais-descricao"
+                                     class="bi bi-pencil"></i>
+                                 </button>
                           </div> 
                           </td>
                          <td>
                               <div class="d-flex justify-content-center align-items-center">
-                              <button class="botao-deletar-item" data-item-id="${id}"><i id="icon-mais-descricao"
+                                 <button class="botao-deletar-item" data-item-id="${id}"><i id="icon-mais-descricao"
                                      class="bi bi-trash3"></i>
                                  </button>
                           </div> 
                           </td>
                     `;
-
-
-                    const botaoDeletarItem = novaLinha.querySelector('.botao-deletar-item');
-                    botaoDeletarItem.addEventListener('click', () => confirmarDeletar(botaoDeletarItem.dataset.itemId));
-                
-                    return novaLinha;
-                }
-                
-                function confirmarDeletar(itemId) {
-                    Swal.fire({
-                        title: 'Remoção de item!',
-                        text: 'Você tem certeza que deseja remover o item?',
-                        icon: 'question',
-                        showCancelButton: true, // Adiciona o botão de cancelar
-                        cancelButtonText: 'Cancelar',
-                        confirmButtonText: 'Confirmar',
-                        confirmButtonColor: '#5C7243',
-                        cancelButtonColor: '#A3C977'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            deletarItem(itemId);
-                        }
-                    });
-                }
-
-function deletarItem(itemId) {
+  
+  
+    const botaoDeletarItem = novaLinha.querySelector('.botao-deletar-item');
+    botaoDeletarItem.addEventListener('click', () => confirmarDeletar(botaoDeletarItem.dataset.itemId));
+  
+    return novaLinha;
+  }
+  
+  function confirmarDeletar(itemId) {
+    Swal.fire({
+        title: 'Remoção de item!',
+        text: 'Você tem certeza que deseja remover o item?',
+        icon: 'question',
+        showCancelButton: true, // Adiciona o botão de cancelar
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Confirmar',
+        confirmButtonColor: '#5C7243',
+        cancelButtonColor: '#A3C977'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            deletarItem(itemId);
+        }
+    });
+  }
+  
+  function deletarItem(itemId) {
     // Aqui você faz o fetch para deletar o item com a ID específica
     fetch(`http://localhost:8080/item/deletar/${itemId}`, {
         method: 'DELETE',
@@ -199,95 +198,95 @@ function deletarItem(itemId) {
         .catch(error => {
             console.error('Erro ao deletar item:', error);
         });
-}
-
-const nextPageButton = document.getElementById('next-page');
-if (nextPageButton) {
+  }
+  
+  const nextPageButton = document.getElementById('next-page');
+  if (nextPageButton) {
     nextPageButton.disabled = currentPage === totalPages;
     nextPageButton.style.cursor = currentPage === totalPages ? 'default' : 'pointer';
-
+  
     nextPageButton.addEventListener('click', function (event) {
         event.preventDefault();
-
+  
         if (currentPage < totalPages) {
             carregarItens(sessionStorage.getItem('usuarioId'), currentPage + 1, filtroMesSelecionado, filtroSetorSelecionado);
             currentPage += 1; // Atualiza a variável currentPage
             updateCursorStyle(); // Atualiza o estilo do cursor após mudar de página
         }
     });
-}
-
-const prevPageButton = document.getElementById('prev-page');
-if (prevPageButton) {
+  }
+  
+  const prevPageButton = document.getElementById('prev-page');
+  if (prevPageButton) {
     prevPageButton.disabled = currentPage === 1;
     prevPageButton.style.cursor = currentPage === 1 ? 'default' : 'pointer';
-
+  
     prevPageButton.addEventListener('click', function (event) {
         event.preventDefault();
-
+  
         if (currentPage > 1) {
             carregarItens(sessionStorage.getItem('usuarioId'), currentPage - 1, filtroMesSelecionado, filtroSetorSelecionado);
             currentPage -= 1; // Atualiza a variável currentPage
             updateCursorStyle(); // Atualiza o estilo do cursor após mudar de página
         }
     });
-}
-
-function updateCursorStyle() {
+  }
+  
+  function updateCursorStyle() {
     const nextPageButton = document.getElementById('next-page');
     const prevPageButton = document.getElementById('prev-page');
-
+  
     if (nextPageButton) {
         nextPageButton.disabled = currentPage === totalPages;
         nextPageButton.style.cursor = currentPage === totalPages ? 'default' : 'pointer';
-
-
+  
+  
     }
-
+  
     if (prevPageButton) {
         prevPageButton.disabled = currentPage === 1;
         prevPageButton.style.cursor = currentPage === 1 ? 'default' : 'pointer';
-
+  
     }
-}
-
-// FORMATA A DATA PARA APARECER CONFORME O EXEMPLO : 02/02/2024, COM "0" EM NÚMEROS UNITÁRIOS.
-function formatarData(dataArray) {
+  }
+  
+  // FORMATA A DATA PARA APARECER CONFORME O EXEMPLO : 02/02/2024, COM "0" EM NÚMEROS UNITÁRIOS.
+  function formatarData(dataArray) {
     const [ano, mes, dia] = dataArray;
     const mesFormatado = mes.toString().padStart(2, '0');
     const diaFormatado = dia.toString().padStart(2, '0');
-
+  
     // FORMATA A DATA PARA APARECER CONFORME O EXEMPLO: 02/02/24, SEM O "20" DO ANO.
     if (window.innerWidth <= 460) {
         // Formata a data para "DD/MM/YY"
         return `${diaFormatado}/${mesFormatado}/${ano.toString().slice(-2)}`;
-
+  
     } else {
         return `${diaFormatado}/${mesFormatado}/${ano}`;
     }
-
-}
-
-
-
-
-// Função para verificar e ocultar o menu lateral em telas menores
-function hideMenuOnSmallScreens() {
+  
+  }
+  
+  
+  
+  
+  // Função para verificar e ocultar o menu lateral em telas menores
+  function hideMenuOnSmallScreens() {
     var screenWidth = window.innerWidth;
     var menuLateral = document.getElementById('menuLateral');
     var menuSUPERIOR = document.getElementById('menuSUPERIOR');
     var reduzirDescricaoParaDesc = document.getElementById('reduzir-descricao-para-desc');
     var reduzirEditar = document.getElementById('reduzir-editar');
     var reduzirDeletar = document.getElementById('reduzir-deletar');
-
-
+  
+  
     if (screenWidth <= 460) { // Se a largura da tela for 460 pixels ou menos
         menuLateral.style.display = 'none'; // Oculta o menu lateral
         menuSUPERIOR.style.display = 'block';
         reduzirDescricaoParaDesc.innerText = '';
         reduzirDeletar.innerText = '';
         reduzirEditar.innerText = '';
-
+  
     } else {
         menuLateral.style.display = 'block'; // Exibe o menu lateral
         menuSUPERIOR.style.display = 'none';
@@ -295,12 +294,12 @@ function hideMenuOnSmallScreens() {
         reduzirDeletar.innerText = 'Deletar';
         reduzirEditar.innerText = 'Editar';
     }
-}
-
-// Chama a função quando a página é carregada e quando a janela é redimensionada
-window.onload = hideMenuOnSmallScreens;
-window.onresize = hideMenuOnSmallScreens;
-
-function stopPropagation(event) {
+  }
+  
+  // Chama a função quando a página é carregada e quando a janela é redimensionada
+  window.onload = hideMenuOnSmallScreens;
+  window.onresize = hideMenuOnSmallScreens;
+  
+  function stopPropagation(event) {
     event.stopPropagation();
-}
+  }
